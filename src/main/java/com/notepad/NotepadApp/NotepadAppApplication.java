f308f1a62d61544f17ba2800e07960854d3e11a5
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2020-10-14 23:40:27 +0100

    First Commit

---
+++ src/main/java/com/notepad/NotepadApp/NotepadAppApplication.java
@@ -0,0 +1,13 @@
+package com.notepad.NotepadApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notepad/NotepadApp/NotepadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notepad.NotepadApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
